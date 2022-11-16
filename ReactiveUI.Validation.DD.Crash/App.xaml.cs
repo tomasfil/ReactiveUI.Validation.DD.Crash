66bf63228374661a5effcdaafec2741dfb563218
tomas.filip <tomas.filip@EPCEPC808>
2022-11-16 14:58:16 +0100

    Add project files.

---
+++ ReactiveUI.Validation.DD.Crash/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReactiveUI.Validation.DD.Crash;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
